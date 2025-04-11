bb372cf08df68acd91d79b3674240f3d9ea55a35
Vinicius <vinicius.polo@speedbird.aero>
2025-04-11 18:23:06 -0300

    algoritimo pronto

--- vaiNaWeb/caminho.py
+++ vaiNaWeb/caminho.py
@@ -1,38 +1,69 @@
-import sys
+import math
 
 def mostrar_tabuleiro(tabuleiro):
     for linha in tabuleiro:
-        print("| " + " |".join(linha) + " |")
+        print('| ' + ' | '.join(linha) + ' |')
+    print()
 
-def proximo_movimento(tabuleiro, linha_atual, coluna_atual, profundidade):
-    melhor_profundidade = float("inf")
-    
-    melhor_linha= linha_atual
-    melhor_coluna = coluna_atual
-
-    proxima_linha = linha_atual
-    proxima_coluna = coluna_atual + 1
-
-    if movimento_valido(tabuleiro, proxima_linha, proxima_coluna):
-        if chegou_destino(proxima_linha, proxima_coluna):
-            return proxima_linha, proxima_coluna
-     
 def movimento_valido(tabuleiro, linha, coluna):
-    return tabuleiro
+    return (0 <= linha < len(tabuleiro) and
+            0 <= coluna < len(tabuleiro[0]) and
+            tabuleiro[linha][coluna] == ' ')
 
 def chegou_destino(linha, coluna):
-    return linha == 2 and coluna == 2
+    return linha == 1 and coluna == 4
+
+def encontrar_caminho(tabuleiro, linha_atual, coluna_atual, caminho):
+    if chegou_destino(linha_atual, coluna_atual):
+        caminho.append((linha_atual, coluna_atual))
+        return True
+
+    direcoes = [(0, 1), (0, -1), (-1, 0), (1, 0)]  # direita, esquerda, cima, baixo
+
+    for d_linha, d_coluna in direcoes:
+        nova_linha = linha_atual + d_linha
+        nova_coluna = coluna_atual + d_coluna
+
+        if movimento_valido(tabuleiro, nova_linha, nova_coluna):
+            tabuleiro[nova_linha][nova_coluna] = '°'  # marca como visitado
+            if encontrar_caminho(tabuleiro, nova_linha, nova_coluna, caminho):
+                caminho.append((linha_atual, coluna_atual))
+                return True
+            tabuleiro[nova_linha][nova_coluna] = ' '  # desmarca (backtracking)
+
+    return False
 
 def main():
-    tabuleiro = [[" " for _ in range(3)] for _ in range(3)]
+    tabuleiro = [
+        [' ', ' ', ' ', 'X', 'X'],
+        [' ', 'X', ' ', 'X', ' '],
+        [' ', 'X', ' ', 'X', ' '],
+        [' ', 'X', ' ', ' ', ' ']
+    ]
+    linha_inicio, coluna_inicio = 3, 0
+    caminho = []
+
+    # marca o início
+    tabuleiro[linha_inicio][coluna_inicio] = '*'
+    #tabuleiro[0][3] = '*'
     mostrar_tabuleiro(tabuleiro)
 
-    linha_atual = 0
-    coluna_atual = 0
-    profundidade = 0 
+    if encontrar_caminho(tabuleiro, linha_inicio, coluna_inicio, caminho):
+        caminho.append((linha_inicio, coluna_inicio))  # adiciona o início ao final
+        caminho.reverse()  # inverte o caminho para ordem do início ao fim
 
+        # limpar tabuleiro e desenhar o caminho final
+        for i, linha in enumerate(tabuleiro):
+            for j, valor in enumerate(linha):
+                if valor == '':
+                    tabuleiro[i][j] = '°'
+        for linha, coluna in caminho:
+            tabuleiro[linha][coluna] = '°'
 
+        print("Caminho encontrado:")
+        mostrar_tabuleiro(tabuleiro)
+    else:
+        print("Não foi possível encontrar um caminho.")
 
-   
-    
-
+if __name__ == "__main__":
+    main()
